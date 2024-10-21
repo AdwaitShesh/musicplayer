310faaadae4d4c4829e6264293f8c9659a20bc96
AdwaitShesh <adwaitshesh04@gmail.com>
2024-10-21 20:06:57 +0530

    display added

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Routes ,Route} from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import DisplayAlbum from './DisplayAlbum'
+// display
 
 const Display = () => {
   return (
