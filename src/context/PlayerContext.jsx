2fd391933bde7b9747c6d26bdc6d2a168a609df3
AdwaitShesh <adwaitshesh04@gmail.com>
2024-10-23 22:55:07 +0530

    seekbar solved

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -61,7 +61,7 @@ const PlayerContextProvider = (props)=>{
     useEffect(()=>{
         setTimeout(()=>{
             audioRef.current.ontimeupdate = ()=>{
-                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime / audioRef.current.duration * 100))+"%";
+                seekBar.current.style.width = (Math.floor(audioRef.current.currentTime/audioRef.current.duration*100))+"%";
                 setTime({
                     currentTime:{
                         second:Math.floor(audioRef.current.currentTime % 60),
